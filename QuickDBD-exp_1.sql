895b6fd0cfda8e5f1192fa423d4127277f5848ad
harsh-env <mpddriller@live.com>
2022-06-11 13:40:32 -0500

    upadate

--- QuickDBD-exp_1.sql
+++ QuickDBD-exp_1.sql
@@ -23,4 +23,25 @@ CREATE TABLE "Global_Agri_Greenhouse_Emissions_%" (
 	ALTER TABLE "Global_Agri_Percent_Emissions"
 	RENAME COLUMN "Glb_Agri_Emission_%" TO "Percent_Glb_Agri_Emission";
 	
-	
+	CREATE TABLE "Global_oil_data_iea" (
+    "year" INT   NOT NULL,
+    "month" INT   NOT NULL,
+    "Country" VARCHAR   NOT NULL,
+    "Data_Interval" VARCHAR   NOT NULL,
+    "Data_Category" VARCHAR   NOT NULL,
+    "million(bbls)" FLOAT   NOT NULL
+	);
+
+    CREATE TABLE "Global_oil_demand" (
+    "year" INT   NOT NULL,
+    "month" INT   NOT NULL,
+    "Country" VARCHAR   NOT NULL,
+    "Data_Interval" VARCHAR   NOT NULL,
+    "Data_Category" VARCHAR   NOT NULL,
+    "million(bbls)" FLOAT   NOT NULL
+	);
+
+    ALTER TABLE "Global_oil_demand"
+    RENAME COLUMN "million(bbls)" TO "million-bbls";
+    ALTER TABLE "Global_oil_data_iea"
+    RENAME COLUMN "million(bbls)" TO "million-bbls";
